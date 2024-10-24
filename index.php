1a917348af7b84c11709c6f6e6ec4bb106fa21dc
counterfitninja <dave@chaoticblue.co.uk>
2024-10-24 11:43:22 +0100

    go

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?phpinfo();
+
+$extPath = ini_get("extension_dir");
+print "Extension Dir: " . $extPath . "\n";
+
+
+$incPath = ini_get("include_path");
+print "Include Path: " . $incPath . "\n";
+
+
+
 ?>
